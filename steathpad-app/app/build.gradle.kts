03f28298b5a5a44e5eb26bf76cb39f328e083446
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-06-16 23:49:37 +0530

    Add Hilt/Room/lifecycle deps and update Gradle
    Enable Hilt DI and common Android libraries, update project/IDE and Gradle settings.
    
    - Add Hilt plugin and dependencies (hilt-android, hilt-compiler) and register plugin in top-level build file.
    - Add Room, Lifecycle (ViewModel/LiveData), UI libs (RecyclerView, ConstraintLayout, Lottie) and enable viewBinding in module build.gradle.kts.
    - Simplify compileSdk to 36 and update AndroidManifest: set application class (.StealthPadApp), replace splash activity, add DashboardActivity and AddNotes entries.
    - Update gradle.properties to enable AndroidX/Jetifier.
    - Change AGP version in libs.versions.toml and bump Gradle wrapper to 9.4.1.
    - Minor IDE metadata changes: add PlanningMode.xml, GradleMigrationSettings, and tweak ProjectRootManager setting.

--- steathpad-app/app/build.gradle.kts
+++ steathpad-app/app/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.kavya.stealthpad"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.kavya.stealthpad"
@@ -33,6 +30,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -41,4 +43,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Room DB....
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // Livedata + ViewModel + Lifecycle....
+    val lifecycle_version = "2.7.0"
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime:$lifecycle_version")
+
+    // UI
+    val lottieVersion = "6.7.1"
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.airbnb.android:lottie:$lottieVersion")
+
+    // DI (Hilt Dagger Dependency)
+    implementation("com.google.dagger:hilt-android:2.57")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.57")
 }
