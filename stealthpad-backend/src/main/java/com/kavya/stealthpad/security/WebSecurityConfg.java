9428639291bcb6080274d732966332da29fc950f
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-06-28 23:56:56 +0530

    Add JWT authentication filter to security chain
    Integrate JWT authentication into Spring Security configuration by injecting JwtAuthenticationFilter and adding it to the security filter chain. This enables JWT-based authentication for the application.

--- stealthpad-backend/src/main/java/com/kavya/stealthpad/security/WebSecurityConfg.java
+++ stealthpad-backend/src/main/java/com/kavya/stealthpad/security/WebSecurityConfg.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebSecurityConfg {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
        http.csrf(csrfconfig -> csrfconfig.disable())
@@ -21,8 +24,8 @@ public class WebSecurityConfg {
                             sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                             .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
                             .permitAll()
-                            .anyRequest().authenticated());                            
-                            ;
+                            .anyRequest().authenticated())
+                            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);;
         return http.build();
     }
 
